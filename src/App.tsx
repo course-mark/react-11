5299d89c8beb9454bc29e5bc6d907d4ee912916f
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-10-25 12:11:46 +0530

    Refactor car details rendering and add new Users page

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,77 @@ import CarDetailsCard from "./components/CarDetailsCard";
 import Greeting from "./components/Greeting";
 import HelloWorld from "./components/HelloWorld";
 
-// React functional Component
+const lordAltoDetails = {
+  carName: "Lord Alto",
+  color: "white",
+  carPhoto:
+    "https://5.imimg.com/data5/VK/AF/MY-26895595/maruti-alto-800-1000x1000.png",
+  size: "3,430 mm L x 1,490 mm W x 1,475 mm H",
+  tagline: "Let's Go",
+  model: "Alto 800",
+  engineCC: "796cc",
+  engineCylinder: "L-3",
+  safetyRating: "3-star",
+  textColor: "text-gray-700",
+};
+
+const tharDetails = {
+  carName: "THAR",
+  color: "black",
+  carPhoto: "https://wallpapercave.com/wp/wp3324581.jpg",
+  size: "3,430 mm L x 1,490 mm W x 1,475 mm H",
+  tagline: "Live young live free",
+  model: "Thar new",
+  engineCC: "999 to 2184 cc",
+  engineCylinder: "L-4",
+  safetyRating: "5-star",
+  textColor: "text-blue-700",
+};
+
+const rubiconDetails = {
+  carName: "Rubicon",
+  color: "red",
+  carPhoto: "https://cdn1.smartprix.com/rx-i5rYmPI57-w1200-h1200/5rYmPI57.jpg",
+  size: "3,430 mm L x 1,490 mm W x 1,475 mm H",
+  tagline: "Live young live free",
+  model: "Thar new",
+  engineCC: "999 to 2184 cc",
+  engineCylinder: "L-4",
+  safetyRating: "5-star",
+  textColor: "text-blue-700",
+};
+
+// array of objects
+const cars = [tharDetails, lordAltoDetails, rubiconDetails, tharDetails];
+
 function App() {
+
+  // map function converts array of objects(cars) into array of JSX(carsUI)
+  const carsUI = cars.map((car) => {
+    return (
+      <CarDetailsCard
+        carName={car.carName}
+        color={car.color}
+        carPhoto={car.carPhoto}
+        size={car.size}
+        tagline={car.tagline}
+        model={car.model}
+        engineCC={car.engineCC}
+        engineCylinder={car.engineCylinder}
+        safetyRating={car.safetyRating}
+        textColor={car.textColor}
+      />
+    );
+  });
+
   return (
     <>
       <HelloWorld />
       <div>
-        <Greeting greetingName="Good Morning" language="english"/>
-        <CarDetailsCard carName="Lord Alto" color="white" carPhoto="https://preview.redd.it/never-knew-lord-alto-have-a-4x4-varient-v0-77qhque0ct7c1.png?auto=webp&s=312faf1d7d20bf3c51fa8e5be784ca1373dff7af"/>
+        <Greeting greetingName="Good Morning" language="english" />
+        <div className="grid grid-cols-3">
+          { carsUI }
+        </div>
       </div>
     </>
   );
--- src/components/CarDetailsCard.tsx
+++ src/components/CarDetailsCard.tsx
@@ -10,6 +10,7 @@ const CarDetailsCard = (props) => {
   const engineCC = props.engineCC;
   const engineCylinder = props.engineCylinder;
   const safetyRating = props.safetyRating
+  const textColor = props.textColor;
   return (
     <div className="flex px-3 py-3">
       <div className="max-w-sm rounded overflow-hidden shadow-lg">
@@ -20,22 +21,38 @@ const CarDetailsCard = (props) => {
         />
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{carName}</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
+          <p className={"text-base "+textColor}>
+          The Alto originated as a commercial vehicle derivative of the Fronte, 
+          but over time the Alto nameplate gained in popularity and by 1988
+           it replaced the Fronte name completely.
           </p>
         </div>
         <div className="px-6 py-4">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
             {color}
           </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #travel
+          
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr2">
+            {tagline}
           </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-            #winter
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-4">
+            {model}
           </span>
+
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2">
+            {size}
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2  ">
+            {engineCC}
+          </span>
+
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2 mr-2">
+            {engineCylinder}
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2 mr-2">
+            {safetyRating}
+          </span>
+          
         </div>
       </div>
     </div>
