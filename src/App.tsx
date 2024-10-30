7fd98d63184ba4322d8da9b82fd1ffa8a9b1f6ac
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-10-30 11:57:44 +0530

    Refactor package.json and App.tsx to add react-router-dom dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,33 @@
-import "./App.css";
-import CarDetailsCard from "./components/CarDetailsCard";
-import Greeting from "./components/Greeting";
-import HelloWorld from "./components/HelloWorld";
+import React from "react";
+import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Users from "./pages/Users";
+/**
+ * 1. RouterProvider
+ * 2. create router using createBrowserRouter
+ */
 
-const lordAltoDetails = {
-  carName: "Lord Alto",
-  color: "white",
-  carPhoto:
-    "https://5.imimg.com/data5/VK/AF/MY-26895595/maruti-alto-800-1000x1000.png",
-  size: "3,430 mm L x 1,490 mm W x 1,475 mm H",
-  tagline: "Let's Go",
-  model: "Alto 800",
-  engineCC: "796cc",
-  engineCylinder: "L-3",
-  safetyRating: "3-star",
-  textColor: "text-gray-700",
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world</div>,
+  },
+  {
+    path:'/singers',
+    element: <Users/>,
+  },
+  {
+    path: '/singers/karan-aujla',
+    element: <div>Hello from Karan</div>,
+  }
+]);
 
-const tharDetails = {
-  carName: "THAR",
-  color: "black",
-  carPhoto: "https://wallpapercave.com/wp/wp3324581.jpg",
-  size: "3,430 mm L x 1,490 mm W x 1,475 mm H",
-  tagline: "Live young live free",
-  model: "Thar new",
-  engineCC: "999 to 2184 cc",
-  engineCylinder: "L-4",
-  safetyRating: "5-star",
-  textColor: "text-blue-700",
-};
-
-const rubiconDetails = {
-  carName: "Rubicon",
-  color: "red",
-  carPhoto: "https://cdn1.smartprix.com/rx-i5rYmPI57-w1200-h1200/5rYmPI57.jpg",
-  size: "3,430 mm L x 1,490 mm W x 1,475 mm H",
-  tagline: "Live young live free",
-  model: "Thar new",
-  engineCC: "999 to 2184 cc",
-  engineCylinder: "L-4",
-  safetyRating: "5-star",
-  textColor: "text-blue-700",
-};
-
-// array of objects
-const cars = [tharDetails, lordAltoDetails, rubiconDetails, tharDetails];
-
-function App() {
-
-  // map function converts array of objects(cars) into array of JSX(carsUI)
-  const carsUI = cars.map((car) => {
-    return (
-      <CarDetailsCard
-        carName={car.carName}
-        color={car.color}
-        carPhoto={car.carPhoto}
-        size={car.size}
-        tagline={car.tagline}
-        model={car.model}
-        engineCC={car.engineCC}
-        engineCylinder={car.engineCylinder}
-        safetyRating={car.safetyRating}
-        textColor={car.textColor}
-      />
-    );
-  });
-
+const App = () => {
   return (
-    <>
-      <Users/>
-    </>
+    <RouterProvider router={router} >
+      
+    </RouterProvider>
   );
-}
+};
 
 export default App;
