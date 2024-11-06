3879af557eb492e04a060d8c6e6883d891c00d88
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-11-06 11:53:45 +0530

    Enhance Events component with mouse hover effects and dynamic background color

--- src/pages/concepts/Events.tsx
+++ src/pages/concepts/Events.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Events = () => {
+  const [isMouseIn, setIsMouseIn] = useState(false)
+
+  const bgColor = isMouseIn? "bg-slate-100" : "bg-slate-900"
+  
   return (
-    <div className="h-[100vh] bg-slate-100 flex flex-col space-y-5 items-center justify-center">
+    <div className={`h-[100vh] ${bgColor} flex flex-col space-y-5 items-center justify-center ease-in duration-300`}>
       <div
         onClick={() => {
           console.log(new Date());
@@ -14,17 +18,27 @@ const Events = () => {
       <div
         onMouseEnter={()=>{
           console.log('mouse entered')
+          setIsMouseIn(true)
         }}
         onMouseLeave={()=>{
           console.log('mouse left')
+          setIsMouseIn(false)
         }}
         onMouseMove={()=>{
           console.log('mouse is moving', new Date())
 
         }}
-        className=" cursor-pointer text-4xl bg-slate-300 w-96 h-96 flex items-center justify-center rounded-2xl text-white"
+        className=" cursor-pointer text-4xl bg-slate-300 w-96 h-96 flex flex-col items-center justify-center rounded-2xl text-white"
       >
         <div>Hover Events</div>
+        {
+          isMouseIn? (
+            <> 🐁 </>
+          ): (
+            
+            <>  </>
+          )
+        }
       </div>
     </div>
   );
