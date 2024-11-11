a9d98c21eb31338d165d83521a808acc336ac44f
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-11-11 14:22:39 +0530

    Refactor Layout component by separating Header and Footer into individual files

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+    return (
+      <footer className="bg-gray-100">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-center space-x-6">
+            <a href="#" className="text-gray-300 hover:text-white">
+              Home
+            </a>
+            <a href="#" className="text-gray-300 hover:text-white">
+              About
+            </a>
+            <a href="#" className="text-gray-300 hover:text-white">
+              Contact
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  };
+
+
+export default Footer;
