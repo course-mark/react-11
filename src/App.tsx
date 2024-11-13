cfd19685f7854607977f488bd9119f82d67c17d6
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-11-13 11:48:59 +0530

    Refactor Header component for improved styling and make it sticky

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
       {
         path: "/harbhajan-maan",
         element: <HarbhajanMaan />,
-      }
+      },
     ],
   },
 ]);
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,51 +1,51 @@
 import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-      <nav className="bg-gray-800">
-        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-          <div className="relative flex items-center justify-between h-16">
-            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="flex-shrink-0 flex items-center"></div>
-              <div className="hidden sm:block sm:ml-6">
-                <div className="flex space-x-4">
-                  <Link
-                    to="/"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Home
-                  </Link>
-                  <Link
-                    to="/about-us"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    About
-                  </Link>
-                  <Link
-                    to="/karan-aujla"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    karan aujla
-                  </Link>
-                  <Link
-                    to="/ranjit-bawa"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Ranjit Bawa
-                  </Link>
-                  <Link
-                    to="/harbhajan-maan"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Harbhajan Maan
-                  </Link>
-                </div>
+  return (
+    <nav className="bg-gray-800 sticky top-0">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center"></div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <Link
+                  to="/"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="/about-us"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  About
+                </Link>
+                <Link
+                  to="/karan-aujla"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  karan aujla
+                </Link>
+                <Link
+                  to="/ranjit-bawa"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Ranjit Bawa
+                </Link>
+                <Link
+                  to="/harbhajan-maan"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Harbhajan Maan
+                </Link>
               </div>
             </div>
           </div>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
+}
 
-  export default Header;
+export default Header;
