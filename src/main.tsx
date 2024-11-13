8a6d6c8070d77e094d86e8355c3e88a9daf75b7a
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-11-13 11:49:36 +0530

    Remove StrictMode wrapper from main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import Singers from './pages/Singers.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('rooti')!).render(
-  // <StrictMode>
-    <App />
-  // </StrictMode>,
-)
+createRoot(document.getElementById("rooti")!).render(<App />);
