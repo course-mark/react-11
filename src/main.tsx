0b78f013522e154d193d1630e3cb5e8f1e634b6f
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-12-10 12:27:58 +0530

    Refactor rendering logic in main.tsx to use a separate variable for the virtual DOM

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("rooti")!).render(<App />);
+const realDom = document.getElementById("rooti")!;
+
+const virtualDom = createRoot(realDom)
+
+virtualDom.render(<App />);
