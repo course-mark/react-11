e6da985d76cc17130301b9e0cb6c5afeb2aa1525
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-12-10 12:28:25 +0530

    Implement visibility toggle and input handling in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,34 @@
 // created by raman
 
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  console.log('Rerendering Home')
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    // alert("Hello World from Home")
+    setTimeout(() => {
+      setVisible(!visible);
+    }, 2000);
+    return () => {
+      // alert("Bye Bye")
+    };
+  }, []);
+
+  const [text, setText] = useState("");
+
+  useEffect(()=>{
+    console.log("yoyo")
+  },[text])
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      Hello world from Home Page
+      {text.length % 2 ? "Hello world from Home Page" : ""}
+      <input
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
     </div>
   );
 };
